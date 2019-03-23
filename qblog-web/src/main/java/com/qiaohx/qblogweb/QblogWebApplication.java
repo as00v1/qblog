3270f0b29003828c9324241bc7a166aab3c2b73a
jiayong <36148963@qq.com>
2019-03-23 15:00:09 +0800

    Initial commit

---
+++ qblog-web/src/main/java/com/qiaohx/qblogweb/QblogWebApplication.java
@@ -0,0 +1,13 @@
+package com.qiaohx.qblogweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QblogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QblogWebApplication.class, args);
+    }
+
+}
