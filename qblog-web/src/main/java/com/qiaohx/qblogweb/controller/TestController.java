b8386447deaf01d3a7a08a963eaf4824dd5b1df2
jiayong <36148963@qq.com>
2019-03-23 16:11:01 +0800

    配置日志

--- qblog-web/src/main/java/com/qiaohx/qblogweb/controller/TestController.java
+++ qblog-web/src/main/java/com/qiaohx/qblogweb/controller/TestController.java
@@ -1,14 +1,14 @@
 package com.qiaohx.qblogweb.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
     @RequestMapping("/qblog")
-    public String test(){
+    public String qblog(){
         return "hello qblog";
     }
+
 }
