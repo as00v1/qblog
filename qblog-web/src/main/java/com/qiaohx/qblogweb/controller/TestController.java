261492ac75eefbc7da591a2917ba95d3f8f7277f
jiayong <36148963@qq.com>
2019-03-24 21:37:31 +0800

    修改依赖

--- qblog-web/src/main/java/com/qiaohx/qblogweb/controller/TestController.java
+++ qblog-web/src/main/java/com/qiaohx/qblogweb/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.qiaohx.qblogweb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @RequestMapping("/qblog")
     public String qblog(){
