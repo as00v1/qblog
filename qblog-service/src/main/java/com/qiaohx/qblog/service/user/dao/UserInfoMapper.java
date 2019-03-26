ec6f29814fd3010e65de5e34efadb8f7770f5207
jiayong <36148963@qq.com>
2019-03-26 09:08:44 +0800

    重新生成映射

--- qblog-service/src/main/java/com/qiaohx/qblog/service/user/dao/UserInfoMapper.java
+++ qblog-service/src/main/java/com/qiaohx/qblog/service/user/dao/UserInfoMapper.java
@@ -3,7 +3,15 @@ package com.qiaohx.qblog.service.user.dao;
 import com.qiaohx.qblog.service.user.model.UserInfo;
 
 public interface UserInfoMapper {
+    int deleteByPrimaryKey(String userId);
+
     int insert(UserInfo record);
 
     int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
 }
