c3c6b841a528ad3b6e0ad8d31fcd35bd71806816
jiayong <36148963@qq.com>
2019-04-27 16:05:09 +0800

    完善七牛云图片上传

--- qblog-web/src/main/java/com/qiaohx/qblog/qblogweb/controller/upload/UploadController.java
+++ qblog-web/src/main/java/com/qiaohx/qblog/qblogweb/controller/upload/UploadController.java
@@ -1,9 +1,17 @@
 package com.qiaohx.qblog.qblogweb.controller.upload;
 
+import com.google.gson.Gson;
 import com.qiaohx.qblog.api.upload.vo.SingleUploadResponseVo;
 import com.qiaohx.qblog.service.common.sequence.SequenceUtil;
 import com.qiaohx.util.response.ErrorCodeEnums;
 import com.qiaohx.util.response.ResponseUtil;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -28,6 +36,18 @@ public class UploadController {
 
     @Value("${filepath}")
     private String fileBasePath;
+
+    @Value("${qiniu.appkey}")
+    private String qiniuAppkey;
+
+    @Value("${qiniu.appsecret}")
+    private String qiniuAppsecret;
+
+    @Value("${qiniu.bucket}")
+    private String qiniuBucket;
+
+    @Value("${qiniu.baseurl}")
+    private String qiniuBaseurl;
 
     @RequestMapping(value = "/singleUpload", method = RequestMethod.POST)
     @ResponseBody
@@ -56,13 +76,24 @@ public class UploadController {
                     return ResponseUtil.result(ErrorCodeEnums.FILE_ERROR, SingleUploadResponseVo.class);
                 }
             }
-            os = new FileOutputStream(new File(fileSavePath));
+            File imageFile = new File(fileSavePath);
+            os = new FileOutputStream(imageFile);
             byte[] b = new byte[1024];
             while (is.read(b) > 0){
                 os.write(b);
             }
 
             os.flush();
+
+            String url = uploadQiniu(fileSavePath);
+            if (!"".equals(url)){
+                boolean delFlag = imageFile.delete();// 删除
+                SingleUploadResponseVo singleUploadResponseVo = ResponseUtil.success(SingleUploadResponseVo.class);
+                singleUploadResponseVo.setUrl(url);
+                return singleUploadResponseVo;
+            }else {
+                return ResponseUtil.result(ErrorCodeEnums.FILE_ERROR, SingleUploadResponseVo.class);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -79,11 +110,41 @@ public class UploadController {
                 logger.error("关闭IS异常", e);
             }
         }
+    }
 
+    /**
+     * 上传到七牛
+     * @param filePath 本地路径
+     * @return 七牛地址
+     */
+    private String uploadQiniu(String filePath){
 
-        SingleUploadResponseVo singleUploadResponseVo = ResponseUtil.success(SingleUploadResponseVo.class);
-        singleUploadResponseVo.setUrl(fileSavePath);
-        return singleUploadResponseVo;
+        //构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone1());
+        //...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+        //...生成上传凭证，然后准备上传
+
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = null;
+        Auth auth = Auth.create(qiniuAppkey, qiniuAppsecret);
+        String upToken = auth.uploadToken(qiniuBucket);
+        try {
+            Response response = uploadManager.put(filePath, key, upToken);
+            //解析上传成功的结果
+            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+            logger.info("putRet.key == " + putRet.key);
+            logger.info("putRet.hash == " + putRet.hash);
+            return qiniuBaseurl.concat(putRet.key);
+        } catch (QiniuException ex) {
+            Response r = ex.response;
+            logger.error(r.toString());
+            try {
+                logger.error(r.bodyString());
+            } catch (QiniuException ex2) {
+            }
+        }
+        return "";
     }
 
 }
