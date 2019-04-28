6e7f15e7faba87341f20549e922f2a62e71f65ae
jiayong <36148963@qq.com>
2019-04-28 13:49:05 +0800

    博客跳转

--- qblog-web/src/main/java/com/qiaohx/qblog/qblogweb/controller/blog/BlogController.java
+++ qblog-web/src/main/java/com/qiaohx/qblog/qblogweb/controller/blog/BlogController.java
@@ -1,9 +1,9 @@
 package com.qiaohx.qblog.qblogweb.controller.blog;
 
+import com.qiaohx.qblog.api.blog.service.BlogInfoService;
 import com.qiaohx.qblog.api.blog.service.UserBlogSelectService;
-import com.qiaohx.qblog.api.blog.vo.ArticleAddResponseVo;
+import com.qiaohx.qblog.api.blog.vo.BlogInfoVo;
 import com.qiaohx.qblog.api.blog.vo.UserBlogRelVo;
-import com.qiaohx.qblog.api.user.vo.UserInfoResponseVo;
 import com.qiaohx.util.response.ErrorCodeEnums;
 import com.qiaohx.util.response.ResponseUtil;
 import io.swagger.annotations.ApiOperation;
@@ -26,21 +26,37 @@ public class BlogController {
 
     private final static Logger logger = LoggerFactory.getLogger(BlogController.class);
 
+    private final static String BLOG_BASE_URL = "https://www.qiaohx.com/";
     private final UserBlogSelectService userBlogSelectService;
 
+    private final BlogInfoService blogInfoService;
+
     @Autowired
-    public BlogController(UserBlogSelectService userBlogSelectService) {
+    public BlogController(UserBlogSelectService userBlogSelectService, BlogInfoService blogInfoService) {
         this.userBlogSelectService = userBlogSelectService;
+        this.blogInfoService = blogInfoService;
     }
 
-    @RequestMapping("/{blogTag}")
-    public void goBlog(@PathVariable(name="blogTag") String blogTag,
+    @RequestMapping(value = "/{blogId}", method = RequestMethod.GET)
+    public void goBlog(@PathVariable(name="blogId") String blogId,
                        HttpServletRequest request, HttpServletResponse response){
         try {
-            response.sendRedirect("https://www.qiaohx.com");
-        } catch (IOException e) {
+            BlogInfoVo blogInfoVo = blogInfoService.getBlogInfo(blogId);
+            if (blogInfoVo.getCode() != 0){
+                logger.info("未查询到博客信息");
+                response.sendRedirect(BLOG_BASE_URL + 404);
+                return;
+            }
+            response.sendRedirect(BLOG_BASE_URL + blogId);
+        } catch (Exception e) {
             e.printStackTrace();
-            logger.error("重定向到博客异常 " + blogTag, e);
+            logger.error("重定向到博客异常 " + blogId, e);
+            try {
+                response.sendRedirect(BLOG_BASE_URL + 500);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                logger.error("重定向到博客500异常 " + blogId, e);
+            }
         }
     }
 
